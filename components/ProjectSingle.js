83edda2c4d7049fdc4e86c66f3c0060195986857
Yoann <59776631+MaulineYoann@users.noreply.github.com>
2022-04-16 17:11:56 +0200

    css card

--- components/ProjectSingle.js
+++ components/ProjectSingle.js
@@ -1,60 +1,82 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import styled from 'styled-components'
-import Image from 'next/image'
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+import Button from './Button';
 
-
-export const ProjectSingle = ({project}) => {
-
-    const Contain = styled.div`
-        border: 3px solid #000;
-        background-color: #000;
-        max-height: 500px;
-        max-width: 500px;
-        display: flex;
-        flex-direction: column;
-        overflow: hidden;
-        border-radius: 15px;
-        .desc {
-            padding: 30px;
-            font-size: 1.4rem;
-            border-bottom: 2px solid #fff;
+export const ProjectSingle = ({ project }) => {
+  const Contain = styled.div`
+    /* border: 3px solid #000; */
+    /* height: 450px; */
+    max-width: 400px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    border-radius: 15px;
+    color: #000;
+    background-color: #fff;
+    box-shadow: 21px 10px 13px -8px rgba(0,0,0,0.45);
+    .infoProject {
+        padding: 20px 15px 30px 15px;
+        h4 {
+            text-align: center;
+            font-size: 1.6rem;
+            font-weight: bold;
+            color: #0077b6;
+            margin: 3%;
         }
-        .containButton {
+        .desc {
+            color: #000;
+            font-weight: bold;
+            padding: 0 15px;
+            font-size: 1.5rem;
+        }
+    }
+    .containButton {
+         padding: 15px;
+         background-color: #000;
+         display: flex;
+         width: auto;
+         .contain-link {
+            width: 50%;
             display: flex;
             justify-content: center;
-            gap: 30px;
-            margin: 20px 0;
-            button {
-                height: 35px;
-                width: 150px;
-                color: #fff;
-                font-size: 1.3rem;
-                border-radius: 4px;
-                cursor: pointer;
-                background-color: #0077b6;
-                box-shadow: 5px 5px 5px #0077b6;
-            }
-        }
-    `
+            align-items: center;
+            transition: 300ms;
+            a {
+                color: #0077b6;
+                font-size: 1.4rem;
+                font-weight: bold;
+                letter-spacing: 1.5px;
+                &:hover {
+                    transform: scale(1.2);
+                }
+             }
+         }
+    }
+  `;
   return (
     <Contain>
-        <Image 
-        src={project.image} 
+      <Image
+        src={project.image}
         max-height={300}
         max-width={300}
-        alt={project.name} 
-        />
-        <p className='desc'>{project.description}</p>
-        <div className='containButton'>
-            <button>
-                <a href={project.github} target="_blank">Code Github</a>
-            </button>
-            <button>
-                <a href={project.web} target='_blank'>Site Web</a>
-            </button>
-        </div>
+        alt={project.name}
+      />
+      <div className='infoProject'>
+          <h4>{project.techno}</h4>
+          <p className='desc'>{project.description}</p>
+      </div>
+      <div className="containButton">
+          <div className="contain-link">
+          <a href={project.github}>Github</a>
+          </div>
+          <div className="contain-link">
+          <a href={project.web}>Site Web</a>
+          </div>
+      </div>
+      
     </Contain>
-  )
-}
+  );
+};
--- data/fetchProject.js
+++ data/fetchProject.js
@@ -8,7 +8,7 @@ export const projectDetail = [
     {
         name: 'Sneakers Addict',
         description: `Réalisation d'un site e-commerce développer pour les amoureux de sneakers.`,
-        techno: ['react'],
+        techno: 'React',
         image: sneaker,
         web: 'https://sneakers-addict-a8f97.web.app/',
         github: 'https://github.com/MaulineYoann/sneackers-addict'
@@ -16,23 +16,23 @@ export const projectDetail = [
     {
         name: 'DevFlix',
         description: `site web type 'netflix qui trie et affiche une liste de film et série en récupérant les donnés depuis une API.`,
-        techno: ['react'],
+        techno: 'React',
         image: movie,
         web: 'https://wild-movie.netlify.app/',
         github: 'https://github.com/MaulineYoann/movie-app'
     },
     {
         name: 'Wild Pizza',
-        description: 'Landing page factice representant une pizzeria et ses desserts.',
-        techno: ['Html', 'css'],
+        description: 'Landing page factice représentant une pizzeria et ses desserts.',
+        techno: 'Html/Css',
         image: pizza,
         web: 'https://wild-pizza.netlify.app/',
         github: 'https://github.com/MaulineYoann/Wild-pizza',
     },
     {
-        name: 'ludus',
+        name: 'Ludus',
         description: `Location de Gladiateurs: Hackaton de 24H réaliser durant un projet d'école sur le thême passé/futur. `,
-        techno: ['react'],
+        techno: 'React',
         image: ludus,
         web: 'https://lugus-findus-chevalus.netlify.app/',
         github: 'https://github.com/BenSbnt/Les-Esclavagistes'
