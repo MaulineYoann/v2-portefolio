923a81986f8f431a0ae5ee65445aec3535c63fa3
Yoann <59776631+MaulineYoann@users.noreply.github.com>
2022-04-20 23:34:42 +0200

    styled component

--- components/Container.js
+++ components/Container.js
@@ -1,12 +1,13 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+
 export default function Container({ children }) {
   return (
-    <>
+    <div>
       <Navbar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 }
